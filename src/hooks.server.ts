dfa9910bc1fd53309f1d0bff236e13ec560c16bd
Anyrooooo <10919987@my.uvu.edu>
2024-01-10 13:54:05 -0700

    changing OAuth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,21 +1,21 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/core/providers/github"
 
-// import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
+import { GITHUB_DEV_ID, GITHUB_DEV_SECRET } from "$env/static/private"
  
-// export const handle = SvelteKitAuth({
-//   providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
-// })
-
-
-
-const devGithubConfig = { clientId: process.env.GITHUB_DEV_ID, clientSecret: process.env.GITHUB_DEV_SECRET}
-// const prodGithubConfig = { clientId: process.env.GITHUB_PROD_ID, clientSecret: process.env.GITHUB_PROD_SECRET}
-// process.env.NODE_ENV === 'development' ? githubConfig = devGithubConfig : githubConfig = prodGithubConfig
-const githubConfig = devGithubConfig
-
 export const handle = SvelteKitAuth({
-  providers: [ 
-    GitHub(githubConfig)
-  ],
+  providers: [GitHub({ clientId: GITHUB_DEV_ID, clientSecret: GITHUB_DEV_SECRET })],
 })
+
+
+
+// const devGithubConfig = { clientId: process.env.GITHUB_DEV_ID, clientSecret: process.env.GITHUB_DEV_SECRET}
+// // const prodGithubConfig = { clientId: process.env.GITHUB_PROD_ID, clientSecret: process.env.GITHUB_PROD_SECRET}
+// // process.env.NODE_ENV === 'development' ? githubConfig = devGithubConfig : githubConfig = prodGithubConfig
+// const githubConfig = devGithubConfig
+
+// export const handle = SvelteKitAuth({
+//   providers: [ 
+//     GitHub(githubConfig)
+//   ],
+// })
