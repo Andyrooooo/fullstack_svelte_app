9ac1a77490606f26a02c70d4307368ea632c0f34
Anyrooooo <10919987@my.uvu.edu>
2024-01-10 13:42:22 -0700

    redoing the OAuth due to errors in Vercel

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,6 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/core/providers/github"
+
 // import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
  
 // export const handle = SvelteKitAuth({
@@ -7,10 +8,11 @@ import GitHub from "@auth/core/providers/github"
 // })
 
 
-let githubConfig
+
 const devGithubConfig = { clientId: process.env.GITHUB_DEV_ID, clientSecret: process.env.GITHUB_DEV_SECRET}
-const prodGithubConfig = { clientId: process.env.GITHUB_PROD_ID, clientSecret: process.env.GITHUB_PROD_SECRET}
-process.env.NODE_ENV === 'development' ? githubConfig = devGithubConfig : githubConfig = prodGithubConfig
+// const prodGithubConfig = { clientId: process.env.GITHUB_PROD_ID, clientSecret: process.env.GITHUB_PROD_SECRET}
+// process.env.NODE_ENV === 'development' ? githubConfig = devGithubConfig : githubConfig = prodGithubConfig
+const githubConfig = devGithubConfig
 
 export const handle = SvelteKitAuth({
   providers: [ 
--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -7,7 +7,7 @@
 {#if $page.data.session?.user}
     <p>Signed in as {$page.data.session.user.email}</p>
     <button on:click={signOut}>Sign out</button>
-    <img src="https://cdn.pixabay.com/photo/2017/08/11/19/36/vw-2632486_1280.png" />
+    <img src="portfolio_logo.png" />
 {:else}
     <p>Not signed in.</p>
     <button on:click={() => signIn('github')}>Sign in</button>
