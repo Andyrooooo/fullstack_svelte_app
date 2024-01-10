60c8ffbed3b04782e1c2030935e50bc7eaff0c3f
Anyrooooo <10919987@my.uvu.edu>
2024-01-10 12:05:55 -0700

    added production OAuth for Vercel app

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,19 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/core/providers/github"
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
+// import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
  
+// export const handle = SvelteKitAuth({
+//   providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+// })
+
+
+let githubConfig
+const devGithubConfig = { clientId: process.env.GITHUB_DEV_ID, clientSecret: process.env.GITHUB_DEV_SECRET}
+const prodGithubConfig = { clientId: process.env.GITHUB_PROD_ID, clientSecret: process.env.GITHUB_PROD_SECRET}
+process.env.NODE_ENV === 'development' ? githubConfig = devGithubConfig : githubConfig = prodGithubConfig
+
 export const handle = SvelteKitAuth({
-  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+  providers: [ 
+    GitHub(githubConfig)
+  ],
 })
--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,9 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
+import dotenv from 'dotenv';
+dotenv.config();
+
 const config = {
 	extensions: ['.svelte'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
